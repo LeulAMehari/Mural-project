7eab25b1bcc2691de750aeee27df65d56185be20
Andrejs Mudulis <amudulis@blc.edu>
2022-11-08 00:14:06 -0600

    converting the getMonth() method

--- MarshallsRevMethods/Mural/Program.cs
+++ MarshallsRevMethods/Mural/Program.cs
@@ -8,11 +8,30 @@ namespace Mural
     {
         const int MAX_MURALS = 30;
 
+        int mymonth;
+
         public static string[] muralModels = { "L", "S", "A", "C", "O" };
         public static string[] muralTypes = { "Landscape", "Seascape", "Abstract", "Children", "Other" };
 
         string[] customerInterior = new string[MAX_MURALS];
         string[] customerExterior = new string[MAX_MURALS];
+
+        public int MyMonth
+        {
+            get
+            {
+                return mymonth;
+            }
+            set
+            {
+                while (value < 1 || value > 12)
+                {
+                    WriteLine("Enter a Valid month");
+                    int.TryParse(ReadLine(), out mymonth);
+                }
+                mymonth = value;
+            }
+        }
         public string[] CustomerInterior
         {
             get 
@@ -24,6 +43,19 @@ namespace Mural
 
             }
         }
+
+        public static int GetMonth()
+        {
+            Write("Enter Month ");
+            Mural var = new Mural();
+
+            int temp;
+            int.TryParse(ReadLine(), out temp);
+            var.MyMonth = temp;
+            return var.MyMonth;
+        }
+
+
         public void DataEntry()
         {
             Write("Enter Your name ");
